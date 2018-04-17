49e491fe510f8affa3660817440844434d42fa17
Falko Menge <falko.menge@camunda.com>
2018-04-17 19:11:12 +0200

    Add latest version of XSD test tool

--- xsd/test-xsd.sh
+++ xsd/test-xsd.sh
@@ -13,7 +13,16 @@ validate_as_dmn_11() {
 }
 
 validate_as_dmn_12() {
-    sed 's#http://www.omg.org/spec/DMN/20151101/dmn.xsd#http://www.omg.org/spec/DMN/20180521/MODEL/#' "$1" | xmllint --schema "$XSD_DIR/dmn.xsd" --noout -
+    migrate_to_dmn_12 "$1" | xmllint --schema "$XSD_DIR/dmn.xsd" --noout -
+}
+
+migrate_to_dmn_12() {
+    sed \
+        -e 's#http://www.omg.org/spec/DMN/20151101/dmn.xsd#http://www.omg.org/spec/DMN/20180521/MODEL/#' \
+        -e 's#http://www.omg.org/spec/DMN/20151101/DMNDI#http://www.omg.org/spec/DMN/20180521/DMNDI/#' \
+        -e 's#http://www.omg.org/spec/DMN/20151101/DI#http://www.omg.org/spec/DMN/20180521/DI/#' \
+        -e 's#http://www.omg.org/spec/DMN/20151101/DC#http://www.omg.org/spec/DMN/20180521/DC/#'\
+        "$1"
 }
 
 # download the official DMN 1.1 XML schema for comparison
@@ -24,32 +33,35 @@ fi
 echo "DMN 1.1 validation results:" > "$TARGET_DIR/validdmn11.log"
 echo "DMN 1.2 validation results:" > "$TARGET_DIR/validdmn12.log"
 
-# search all DMN files in the current directory and perform a schema validation with the DMN 1.1 and 1.2 schemas
+# recursively search all DMN files in the current directory and perform a schema validation with the DMN 1.1 and 1.2 schemas
 declare -i NUMBER_OF_FILES=0
-for DMN_FILE in $(find '(' -iname '*.dmn*.xml' -o -iname '*.dmn' ')' -type f)
-do
+while IFS= read -r -d '' DMN_FILE; do
     NUMBER_OF_FILES+=1
-    echo $DMN_FILE >> "$TARGET_DIR/validdmn11.log"
-    echo $DMN_FILE >> "$TARGET_DIR/validdmn12.log"
+    echo "$NUMBER_OF_FILES: $DMN_FILE"
+    echo "$DMN_FILE" >> "$TARGET_DIR/validdmn11.log"
+    echo "$DMN_FILE" >> "$TARGET_DIR/validdmn12.log"
     validate_as_dmn_11 "$DMN_FILE" >> "$TARGET_DIR/validdmn11.log" 2>&1
     validate_as_dmn_12 "$DMN_FILE" >> "$TARGET_DIR/validdmn12.log" 2>&1
-done
+done < <(find . '(' -iname '*.dmn*.xml' -o -iname '*.dmn' ')' -type f -print0)
+echo "Performed XML schema validation of $NUMBER_OF_FILES DMN files. Starting result comparison..."
 
 # filter out validation errors about intended changes in DMN 1.2
 grep -vf "$XSD_DIR/test-xsd-ignore-list-11.txt" "$TARGET_DIR/validdmn11.log" > "$TARGET_DIR/validdmn11.filtered.log"
 grep -vf "$XSD_DIR/test-xsd-ignore-list-12.txt" "$TARGET_DIR/validdmn12.log" > "$TARGET_DIR/validdmn12.filtered.log"
 
-# adjust lists of expected elements
+# filter out intended changes to lists of expected elements and namespace URIs
 sed -i \
-    -e 's#: This element is not expected. Expected is one of ( {http://www.omg.org/spec/DMN/20151101/dmn.xsd}elementCollection, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}businessContextElement, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}performanceIndicator, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}organizationUnit ).#: This element is not expected. Expected is one of ( {http://www.omg.org/spec/DMN/20151101/dmn.xsd}elementCollection, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}businessContextElement, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}performanceIndicator, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}organizationUnit, {http://www.omg.org/spec/DMN/20151101/DMNDI}DMNDI ).#' "$TARGET_DIR/validdmn11.filtered.log" \
-    -e 's#: Missing child element(s). Expected is one of ( {http://www.omg.org/spec/DMN/20151101/dmn.xsd}requiredDecision, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}requiredInput ).#: Missing child element(s). Expected is one of ( {http://www.omg.org/spec/DMN/20151101/dmn.xsd}description, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}extensionElements, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}requiredDecision, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}requiredInput ).#' \
-    -e 's#http://www.omg.org/spec/DMN/20151101/dmn.xsd#http://www.omg.org/spec/DMN/20180521/MODEL/#' \
+    -e 's#: This element is not expected. Expected is one of ( {http://www.omg.org/spec/DMN/20151101/dmn.xsd}elementCollection, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}businessContextElement, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}performanceIndicator, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}organizationUnit ).#: This element is not expected. Expected is one of ( {http://www.omg.org/spec/DMN/20180521/MODEL/}elementCollection, {http://www.omg.org/spec/DMN/20180521/MODEL/}businessContextElement, {http://www.omg.org/spec/DMN/20180521/MODEL/}performanceIndicator, {http://www.omg.org/spec/DMN/20180521/MODEL/}organizationUnit, {http://www.omg.org/spec/DMN/20180521/DMNDI/}DMNDI ).#' \
+    -e 's#: This element is not expected. Expected is one of ( {http://www.omg.org/spec/DMN/20151101/dmn.xsd}artifact, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}textAnnotation, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}association, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}elementCollection, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}businessContextElement, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}performanceIndicator, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}organizationUnit ).#: This element is not expected. Expected is one of ( {http://www.omg.org/spec/DMN/20180521/MODEL/}artifact, {http://www.omg.org/spec/DMN/20180521/MODEL/}textAnnotation, {http://www.omg.org/spec/DMN/20180521/MODEL/}association, {http://www.omg.org/spec/DMN/20180521/MODEL/}elementCollection, {http://www.omg.org/spec/DMN/20180521/MODEL/}businessContextElement, {http://www.omg.org/spec/DMN/20180521/MODEL/}performanceIndicator, {http://www.omg.org/spec/DMN/20180521/MODEL/}organizationUnit, {http://www.omg.org/spec/DMN/20180521/DMNDI/}DMNDI ).#' \
+    -e 's#: This element is not expected. Expected is one of ( {http://www.omg.org/spec/DMN/20151101/dmn.xsd}description, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}extensionElements, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}encapsulatedLogic, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}variable, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}knowledgeRequirement, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}authorityRequirement ).#: This element is not expected. Expected is one of ( {http://www.omg.org/spec/DMN/20180521/MODEL/}description, {http://www.omg.org/spec/DMN/20180521/MODEL/}extensionElements, {http://www.omg.org/spec/DMN/20180521/MODEL/}variable, {http://www.omg.org/spec/DMN/20180521/MODEL/}encapsulatedLogic, {http://www.omg.org/spec/DMN/20180521/MODEL/}knowledgeRequirement, {http://www.omg.org/spec/DMN/20180521/MODEL/}authorityRequirement ).#' \
+    -e 's#: Missing child element(s). Expected is one of ( {http://www.omg.org/spec/DMN/20151101/dmn.xsd}requiredDecision, {http://www.omg.org/spec/DMN/20151101/dmn.xsd}requiredInput ).#: Missing child element(s). Expected is one of ( {http://www.omg.org/spec/DMN/20180521/MODEL/}description, {http://www.omg.org/spec/DMN/20180521/MODEL/}extensionElements, {http://www.omg.org/spec/DMN/20180521/MODEL/}requiredDecision, {http://www.omg.org/spec/DMN/20180521/MODEL/}requiredInput ).#' \
+    -e 's#http://www.omg.org/spec/DMN/20151101/dmn.xsd#http://www.omg.org/spec/DMN/20180521/MODEL/#g' \
     "$TARGET_DIR/validdmn11.filtered.log"
 
 # compare the filtered log files
 colordiff "$TARGET_DIR/validdmn11.filtered.log" "$TARGET_DIR/validdmn12.filtered.log"
 
-echo "The comparison of XML validation results for $NUMBER_OF_FILES DMN files is done. If the output above shows any differences, it could indicate an unintended break of backwards compatibility in the DMN XML schema."
+echo "Result comparison is done. If the output above shows any differences between the validation results, it could indicate an unintended break of backwards compatibility in the DMN XML schema."
 echo "For more details compare full unfiltered logs with a diff tool e.g.:"
 echo "meld $TARGET_DIR/validdmn1?.log"
 echo "meld $TARGET_DIR/validdmn1?.filtered.log"
