aa029e29dfa67f050ffe22542ead585ee6675d3b
Falko Menge <falko.menge@camunda.com>
2019-11-06 00:33:08 +0100

    Update test script to DMN 1.3

--- xsd/test-xsd.sh
+++ xsd/test-xsd.sh
@@ -8,20 +8,70 @@ XSD_DIR="$(dirname $0)"
 TARGET_DIR="$XSD_DIR/target"
 mkdir -p $TARGET_DIR
 
+DMN11='http://www.omg.org/spec/DMN/20151101/dmn.xsd'
+DMN12='http://www.omg.org/spec/DMN/20180521/MODEL/'
+DMN13='https://www.omg.org/spec/DMN/20191111/MODEL/'
+DMNDI11='http://www.omg.org/spec/DMN/20151101/DMNDI'
+DMNDI12='http://www.omg.org/spec/DMN/20180521/DMNDI/'
+DMNDI13='https://www.omg.org/spec/DMN/20191111/DMNDI/'
+DI='http://www.omg.org/spec/DMN/20180521/DI/'
+DC='http://www.omg.org/spec/DMN/20180521/DC/'
+FEEL10='http://www.omg.org/spec/FEEL/20140401'
+FEEL12='http://www.omg.org/spec/DMN/20180521/FEEL/'
+FEEL13='https://www.omg.org/spec/DMN/20191111/FEEL/'
+
 validate_as_dmn_11() {
-    sed 's#http://www.omg.org/spec/DMN/20180521/MODEL/#http://www.omg.org/spec/DMN/20151101/dmn.xsd#' "$1" | xmllint --schema "$TARGET_DIR/DMN11.xsd" --noout -
+    migrate_to_dmn_11 "$1" | xmllint --schema "$TARGET_DIR/DMN11.xsd" --noout -
 }
 
 validate_as_dmn_12() {
-    migrate_to_dmn_12 "$1" | xmllint --schema "$XSD_DIR/DMN12.xsd" --noout -
+    migrate_to_dmn_12 "$1" | xmllint --schema "$TARGET_DIR/DMN12.xsd" --noout -
+}
+
+validate_as_dmn_13() {
+    migrate_to_dmn_13 "$1" | xmllint --schema "$XSD_DIR/DMN13.xsd" --noout -
+}
+
+migrate_to_dmn_11() {
+    downgrade_dmn_13_to_dmn_12 "$1" | downgrade_dmn_12_to_dmn_11 -
 }
 
 migrate_to_dmn_12() {
+    downgrade_dmn_13_to_dmn_12 "$1" | upgrade_dmn_11_to_dmn_12 -
+}
+
+migrate_to_dmn_13() {
+    upgrade_dmn_11_to_dmn_12 "$1" | upgrade_dmn_12_to_dmn_13 -
+}
+
+upgrade_dmn_11_to_dmn_12() {
     sed \
-        -e 's#http://www.omg.org/spec/DMN/20151101/dmn.xsd#http://www.omg.org/spec/DMN/20180521/MODEL/#' \
-        -e 's#http://www.omg.org/spec/DMN/20151101/DMNDI#http://www.omg.org/spec/DMN/20180521/DMNDI/#' \
-        -e 's#http://www.omg.org/spec/DMN/20151101/DI#http://www.omg.org/spec/DMN/20180521/DI/#' \
-        -e 's#http://www.omg.org/spec/DMN/20151101/DC#http://www.omg.org/spec/DMN/20180521/DC/#'\
+        -e "s#$DMN11#$DMN12#g" \
+        -e "s#$DMNDI11#$DMNDI12#g" \
+        -e "s#$FEEL10#$FEEL12#g" \
+        "$1"
+}
+
+downgrade_dmn_12_to_dmn_11() {
+    sed \
+        -e "s#$DMN12#$DMN11#g" \
+        -e "s#$FEEL12#$FEEL10#g" \
+        "$1"
+}
+
+upgrade_dmn_12_to_dmn_13() {
+    sed \
+        -e "s#$DMN12#$DMN13#g" \
+        -e "s#$DMNDI12#$DMNDI13#g" \
+        -e "s#$FEEL12#$FEEL13#g" \
+        "$1"
+}
+
+downgrade_dmn_13_to_dmn_12() {
+    sed \
+        -e "s#$DMN13#$DMN12#g" \
+        -e "s#$DMNDI13#$DMNDI12#g" \
+        -e "s#$FEEL13#$FEEL12#g" \
         "$1"
 }
 
@@ -30,24 +80,43 @@ if [ ! -f "$TARGET_DIR/DMN11.xsd" ]; then
     wget 'http://www.omg.org/spec/DMN/20151101/dmn.xsd' --output-document="$TARGET_DIR/DMN11.xsd" -nc
 fi
 
+# download the official DMN 1.2 XML schemas for comparison
+if [ ! -f "$TARGET_DIR/DMN12.xsd" ]; then
+    wget 'https://www.omg.org/spec/DMN/20180505/DMN12.xsd' --output-document="$TARGET_DIR/DMN12.xsd" -nc
+fi
+if [ ! -f "$TARGET_DIR/DMNDI12.xsd" ]; then
+    wget 'https://www.omg.org/spec/DMN/20180505/DMNDI12.xsd' --output-document="$TARGET_DIR/DMNDI12.xsd" -nc
+fi
+if [ ! -f "$TARGET_DIR/DI.xsd" ]; then
+    wget 'https://www.omg.org/spec/DMN/20180505/DI.xsd' --output-document="$TARGET_DIR/DI.xsd" -nc
+fi
+if [ ! -f "$TARGET_DIR/DC.xsd" ]; then
+    wget 'https://www.omg.org/spec/DMN/20180505/DC.xsd' --output-document="$TARGET_DIR/DC.xsd" -nc
+fi
+
 echo "DMN 1.1 validation results:" > "$TARGET_DIR/validdmn11.log"
 echo "DMN 1.2 validation results:" > "$TARGET_DIR/validdmn12.log"
+echo "DMN 1.3 validation results:" > "$TARGET_DIR/validdmn13.log"
 
 # recursively search all DMN files in the current directory and perform a schema validation with the DMN 1.1 and 1.2 schemas
 declare -i NUMBER_OF_FILES=0
 while IFS= read -r -d '' DMN_FILE; do
     NUMBER_OF_FILES+=1
     echo "$NUMBER_OF_FILES: $DMN_FILE"
+    #migrate_to_dmn_13 "$DMN_FILE" | sponge "$DMN_FILE"
     echo "$DMN_FILE" >> "$TARGET_DIR/validdmn11.log"
     echo "$DMN_FILE" >> "$TARGET_DIR/validdmn12.log"
+    echo "$DMN_FILE" >> "$TARGET_DIR/validdmn13.log"
     validate_as_dmn_11 "$DMN_FILE" >> "$TARGET_DIR/validdmn11.log" 2>&1
     validate_as_dmn_12 "$DMN_FILE" >> "$TARGET_DIR/validdmn12.log" 2>&1
+    validate_as_dmn_13 "$DMN_FILE" >> "$TARGET_DIR/validdmn13.log" 2>&1
 done < <(find . '(' -iname '*.dmn*.xml' -o -iname '*.dmn' ')' -type f -print0)
 echo "Performed XML schema validation of $NUMBER_OF_FILES DMN files. Starting result comparison..."
 
 # filter out validation errors about intended changes in DMN 1.2
 grep -vf "$XSD_DIR/test-xsd-ignore-list-11.txt" "$TARGET_DIR/validdmn11.log" > "$TARGET_DIR/validdmn11.filtered.log"
 grep -vf "$XSD_DIR/test-xsd-ignore-list-12.txt" "$TARGET_DIR/validdmn12.log" > "$TARGET_DIR/validdmn12.filtered.log"
+grep -vf "$XSD_DIR/test-xsd-ignore-list-13.txt" "$TARGET_DIR/validdmn13.log" > "$TARGET_DIR/validdmn13.filtered.log"
 
 # filter out intended changes to lists of expected elements and namespace URIs
 sed -i \
@@ -59,7 +128,8 @@ sed -i \
     "$TARGET_DIR/validdmn11.filtered.log"
 
 # compare the filtered log files
-colordiff "$TARGET_DIR/validdmn11.filtered.log" "$TARGET_DIR/validdmn12.filtered.log"
+# colordiff "$TARGET_DIR/validdmn11.filtered.log" "$TARGET_DIR/validdmn12.filtered.log"
+colordiff "$TARGET_DIR/validdmn12.filtered.log" "$TARGET_DIR/validdmn13.filtered.log"
 
 echo "Result comparison is done. If the output above shows any differences between the validation results, it could indicate an unintended break of backwards compatibility in the DMN XML schema."
 echo "For more details compare full unfiltered logs with a diff tool e.g.:"
