9c4e8abf4e61403874f484dfcf2a2d14e30793e1
Falko Menge <falko.menge@camunda.com>
2026-04-30 12:18:16 +0200

    Rename *.xmi & *.mdzip to remove version number as per OMG policy

--- xmi/clean-xmi.sh
+++ xmi/clean-xmi.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sed -i 's/DMN16.xmi/DMN.xmi/' *.xmi # in shortDescription
-sed -i 's/DMNDI15.xmi/DMNDI.xmi/' *.xmi # in shortDescription
 sed -i 's/version 20230324/version 20260504/' *.xmi # in shortDescription
 # namespace without HTTPS
 sed -i 's#xmlns:uml="http://www.omg.org/spec/UML/20131001"#xmlns:uml="http://www.omg.org/spec/UML/20161101"#' *.xmi
